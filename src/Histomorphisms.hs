28bb868a1d621b5423b364405cfbb14f37ae7f6d
Jeff <jeffreydwyer95@hotmail.com>
2018-02-01 15:05:33 -0800

    continuing work on histomorphic knapsack problem

--- src/Histomorphisms.hs
+++ src/Histomorphisms.hs
@@ -6,9 +6,11 @@
 
 module Histomorphisms where
 
+import           Control.Comonad
 import           Control.Comonad.Cofree
 import           Control.Monad
 import           Control.Monad.Free
+import           Data.Functor.Compose
 import           Data.Functor.Foldable
 import           Data.List
 import           Data.Numbers.Primes
@@ -30,20 +32,29 @@ instance Ord Item where
 
 addValue :: Double -> Item -> Item -> Maybe Item
 addValue maxWeight a b = do
-  guard (weight a + weight b <= maxWeight)
-  pure $ Item (value a + value b) (weight a + weight b)
+   guard (weight a + weight b <= maxWeight)
+   pure $ Item (value a + value b) (weight a + weight b)
+
+addToHistory :: Double -> Item -> Cofree (ListF Item) (Maybe Item) -> Maybe Item
+addToHistory maxWeight item =
+  extract . extend f . fmap (addValue maxWeight item =<<)
+  where
+    f :: Cofree (ListF Item) (Maybe Item) -> Maybe Item
+    f (v :< Nil) = undefined
 
 maxCheck maxWeight item =
-      guard (weight item <= maxWeight) *> Just item
+       guard (weight item <= maxWeight) *> Just item
 
-knapsack :: Double -> [Item] -> Maybe Item
-knapsack maxWeight = histo f
-  where
-    f :: ListF Item (Cofree (ListF Item) (Maybe Item)) -> Maybe Item
-    f Nil = Nothing
-    f (Cons item (_ :< Nil)) = maxCheck maxWeight item
-    f (Cons item (rt :< (Cons _ (lt :< _)))) = do
-      let left = addValue maxWeight item =<< lt
-      let right = addValue maxWeight item =<< rt
-      let self = maxCheck maxWeight item
-      liftA2 max left right <|> left <|> right <|> self
+
+
+-- knapsack :: Double -> [Item] -> Maybe Item
+-- knapsack maxWeight = histo f
+--   where
+--     f :: ListF Item (Cofree (ListF Item) (Maybe Item)) -> Maybe Item
+--     f Nil = Nothing
+--     f (Cons item (_ :< Nil)) = maxCheck maxWeight item
+--     f (Cons item (rt :< (Cons _ (lt :< _)))) = do
+--       let left = (addValue maxWeight item =<< lt) <|> lt
+--       let right = (addValue maxWeight item =<< rt) <|> rt
+--       let self = maxCheck maxWeight item
+--       liftA2 max left right <|> left <|> right <|> self
