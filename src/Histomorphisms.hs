b643dfe302634e5b8ad9e51e7086eb65ba9f6238
Jeff <jeffreydwyer95@hotmail.com>
2018-02-02 07:08:39 -0800

    Replaced Item in knapsack problem with tuple

--- src/Histomorphisms.hs
+++ src/Histomorphisms.hs
@@ -23,34 +23,22 @@ import           Numeric.Natural
 import qualified Prelude                      as P
 import           Protolude                    hiding (trace)
 
-data Item = Item
-  { value  :: Double
-  , weight :: Double
-  } deriving (Show)
-
-instance Eq Item where
-  a == b = value a == value b
-
-instance Ord Item where
-  a <= b = value a <= value b
-
-knapsack :: Double -> [Item] -> Maybe Item
+knapsack ::
+     (Num value, Ord value, Num weight, Ord weight)
+  => weight
+  -> [(value, weight)]
+  -> Maybe (value, weight)
 knapsack maxWeight = histo f
   where
-      addValue :: Item -> Item -> Maybe Item
-      addValue a b
-        | (weight a + weight b) <= maxWeight =
-            Just $ Item (value a + value b) (weight a + weight b)
-        | weight a <= weight b = Just b
-        | weight a > weight b = Just a
-        | otherwise = Nothing
+    addValue (valueA, weightA) (valueB, weightB)
+      | (weightA + weightB) <= maxWeight =
+        Just $ (valueA + valueB, weightA + weightB)
+      | weightA <= weightB = Just (valueB, weightB)
+      | otherwise = Nothing
 
-      f :: ListF Item (Cofree (ListF Item) (Maybe Item)) -> Maybe Item
-      f Nil              = Nothing
-      f (Cons item prev) = cata (g item) $ fmap (addValue item =<<) prev
+    g item (_ :< Nil) = guard (snd item <= maxWeight) *> Just item
+    g item (l :< (Cons v r)) =
+      fmap maximum . nonEmpty . catMaybes $ [l, addValue item v, r]
 
-      g :: Item -> CofreeF (ListF Item) (Maybe Item) (Maybe Item) -> Maybe Item
-      g item (_ :< Nil) = guard (weight item <= maxWeight) *> Just item
-      g item (l :< (Cons v r)) =
-        let current = addValue item v
-        in fmap maximum . nonEmpty . catMaybes $ [l, current, r]
+    f Nil              = Nothing
+    f (Cons item prev) = cata (g item) $ fmap (addValue item =<<) prev
